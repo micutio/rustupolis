ac25988d4464da7b431e2a1e0ff9b925518659ed
Michael Wagner <wagner.mchl@googlemail.com>
2017-11-22 00:28:19 +0800

    Implemented: primitive tuplespace based on hashset with insert method for tuples.

--- src/tuple.rs
+++ src/tuple.rs
@@ -3,15 +3,17 @@
 //! Tuple is the base class for everything that can
 //! be put into the tuple space.
 
-
-pub struct Tuple<'a> {
-   id: &'a str,
+/// Not using a reference to string because we want ownership of id.
+#[derive(PartialEq, Eq, Hash)]
+pub struct Tuple {
+   id: String,
 }
 
-impl<'a> Tuple<'a> {
+impl Tuple {
     
     /// Create a tuple.
-        pub fn new(_id: &str) -> Tuple {
-        Tuple { id: _id}
+    pub fn new<S>(id: S) -> Tuple where S : Into<String>{
+        Tuple { id: id.into() }
     }
+
 }
--- src/tuplespace.rs
+++ src/tuplespace.rs
@@ -7,25 +7,28 @@ use std::collections::HashSet;
 
 struct TupleSpace {
     // Naive implementation for now: keep all tuples in a set.
-    //data: HashSet<Tuple>
+    data : HashSet<Tuple>,
 }
 
 impl TupleSpace {
 
-    pub fn put(&self, tup: Tuple) {
+    pub fn new() -> TupleSpace {
+        TupleSpace { data: HashSet::new(), }
+    }
+
+    pub fn put(&mut self, tup: Tuple) {
         trace!("[TupleSpace] put tuple into space");
+        self.data.insert(tup);
     }
 
-    pub fn take(&self, tup: Tuple) -> Tuple {
+    pub fn take(&self, tup: Tuple) -> Option<Tuple> {
         trace!("[TupleSpace] taking tuple from space");
-        let result = Tuple::new("takenTuple");
-        result
+        unimplemented!();
     }
 
-    pub fn read(&self, tup: Tuple) -> Tuple {
+    pub fn read(&self, tup: Tuple) -> Option<Tuple> {
         trace!("[TupleSpace] reading tuple from space");
-        let result = Tuple::new("readTuple");
-        result
+        unimplemented!();
     }
 
 }
