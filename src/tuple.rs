d407a00ba755a72e0684557cc4d0dc78895ddf4a
Michael Wagner <wagner.mchl@googlemail.com>
2017-12-02 22:48:27 +0800

    Added: Docstring to tuple

--- src/tuple.rs
+++ src/tuple.rs
@@ -3,7 +3,12 @@
 //! Tuple is the base class for everything that can
 //! be put into the tuple space.
 
-/// Not using a reference to string because we want ownership of id.
+/// Base tuple for the tuple space.
+///
+/// Properties:
+/// - identifier
+/// - lifetime
+/// - generic number of fiels of generic types
 #[derive(PartialEq, Eq, Hash)]
 pub struct Tuple {
    pub id: String,
