c72ca6663d5faae03931ef7085dbbae56291f3a4
Michael Wagner <wagner.mchl@googlemail.com>
2018-01-07 01:42:46 +0800

    Added: tests for take and read to basic_functions

--- tests/basic_functions.rs
+++ tests/basic_functions.rs
@@ -1,5 +1,6 @@
 extern crate rustupolis;
 
+use rustupolis::tuple::E;
 use rustupolis::tuple::Tuple;
 use rustupolis::tuplespace::TupleSpace;
 
@@ -8,9 +9,48 @@ extern crate log;
 
 #[test]
 fn test_put() {
-    info!("testing basic tuplespace functions");
+    info!("testing basic tuplespace function put()");
     let mut t_space = TupleSpace::new();
-    let tup = Tuple::new("tuple1");
+    let content = E::S("test content".to_string());
+    let lifetime = 387465;
+    let tup = Tuple::new(vec!(content), lifetime);
 
     t_space.put(tup);
+}
+
+#[test]
+fn test_read() {
+    info!{"testing basic tuplespace function read()"}
+    let mut t_space = TupleSpace::new();
+    let tup1 = Tuple::new(vec!(E::D(3.14), E::I(123)), 86567);
+    let tup2 = Tuple::new(vec!(E::D(3.14), E::None), 12390);
+    let tup3 = Tuple::new(vec!(E::D(3.14), E::None), 12390);
+
+    t_space.put(tup1);
+    let tup4 = t_space.read(tup2);
+
+    match tup4 {
+        Some(x) => assert!(tup3.content == x.content),
+        None => assert!(false)
+    }
+}
+
+#[test]
+fn test_take() {
+    info!{"testing basic tuplespace function take()"}
+
+    let mut t_space = TupleSpace::new();
+    let tup1 = Tuple::new(vec!(E::D(3.14), E::I(123)), 86567);
+    let tup2 = Tuple::new(vec!(E::D(3.14), E::None), 12390);
+    let tup3 = Tuple::new(vec!(E::D(3.14), E::None), 12390);
+
+    t_space.put(tup1);
+    assert_eq!(t_space.len(), 1);
+    let tup4 = t_space.take(tup2);
+    assert_eq!(t_space.len(), 0);
+
+    match tup4 {
+        Some(x) => assert!(tup3.content == x.content),
+        None => assert!(false)
+    }
 }
