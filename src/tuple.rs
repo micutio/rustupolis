3fc7ec036540f95dc3292e6d081b63306faee67d
Michael Wagner <wagner.mchl@googlemail.com>
2018-01-07 01:01:52 +0800

    Changed: struct Tuple to properly generic data type

--- src/tuple.rs
+++ src/tuple.rs
@@ -1,7 +1,32 @@
 //! Module Tuple
 //!
-//! Tuple is the base class for everything that can
-//! be put into the tuple space.
+//! Tuple is the basis for everything that can be put into the tuple space.
+
+//#[derive(PartialEq)]
+pub enum E {
+    I(i32),
+    D(f64),
+    S(String),
+    T(Vec<E>),
+    None,
+}
+
+impl PartialEq for E {
+    fn eq(&self, other: &E) -> bool {
+        match (self, other) {
+            (&E::I(ref a), &E::I(ref b)) => a == b,
+            (&E::D(ref a), &E::D(ref b)) => a == b,
+            (&E::S(ref a), &E::S(ref b)) => a == b,
+            (&E::T(ref a), &E::T(ref b)) => a == b,
+            (&E::I(ref _a), &E::None) => true,
+            (&E::D(ref _a), &E::None) => true,
+            (&E::S(ref _a), &E::None) => true,
+            (&E::T(ref _a), &E::None) => true,
+            (&E::None, _) => true,
+            _ => false,
+        }
+    }
+}
 
 /// Base tuple for the tuple space.
 ///
@@ -9,16 +34,17 @@
 /// - identifier
 /// - lifetime
 /// - generic number of fiels of generic types
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq)]
 pub struct Tuple {
-   pub id: String,
+   pub content: E,
+   pub lifetime: u64,
 }
 
 impl Tuple {
 
     /// Create a tuple.
-    pub fn new<S>(id: S) -> Tuple where S : Into<String>{
-        Tuple { id: id.into() }
+    pub fn new(ct: E, lt: u64) -> Tuple {
+        Tuple { content: ct, lifetime: lt }
     }
 
 }
--- src/tuplespace.rs
+++ src/tuplespace.rs
@@ -3,38 +3,57 @@
 //! TSpace is the actual tuple space
 
 use tuple::Tuple;
-use std::collections::HashSet;
 
 pub struct TupleSpace {
     // Naive implementation for now: keep all tuples in a set.
-    data : HashSet<Tuple>,
+    data : Vec<Tuple>,
 }
 
 impl TupleSpace {
 
     pub fn new() -> TupleSpace {
-        TupleSpace { data: HashSet::new(), }
+        TupleSpace { data: Vec::new(), }
     }
 
     pub fn put(&mut self, tup: Tuple) {
         trace!("[TupleSpace] put tuple into space");
-        self.data.insert(tup);
+        self.data.push(tup);
     }
 
-    pub fn take(&self, id: String) -> Option<Tuple> {
+    pub fn take(&mut self, tup: Tuple) -> Option<Tuple> {
         trace!("[TupleSpace] taking tuple from space");
-        for t in self.data.iter() {
-            if t.id == id {
-                let result_tuple = Tuple::new(id);
-                return Some(result_tuple);
+        let mut index = self.data.len();
+        for i in 0..self.data.len() {
+            if tup == self.data[i] {
+                index = i;
+                break;
             }
         }
-        None
+
+        if index < self.data.len() {
+            self.data.remove(index);
+            Some(tup)
+        } else {
+            None
+        }
     }
 
     pub fn read(&self, tup: Tuple) -> Option<Tuple> {
         trace!("[TupleSpace] reading tuple from space");
-        unimplemented!();
+
+        let mut index = self.data.len();
+        for i in 0..self.data.len() {
+            if tup == self.data[i] {
+                index = i;
+                break;
+            }
+        }
+
+        if index < self.data.len() {
+            Some(tup)
+        } else {
+            None
+        }
     }
 
 }
