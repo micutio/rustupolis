fa44ab349655723cf0aa7e03ebe4da0ca9fcca03
Michael Wagner <wagner.mchl@googlemail.com>
2018-01-09 16:23:58 +0800

    Added: doc comments

--- src/tuple.rs
+++ src/tuple.rs
@@ -2,12 +2,54 @@
 //!
 //! Tuple is the basis for everything that can be put into the tuple space.
 
+/// Enum, which is used to represent the tuple elements.
 pub enum E {
+    /// Integer data type.
+    ///
+    /// Implemented as 32-bit integer (i32).
     I(i32),
+    /// Floating point data type.
+    ///
+    /// Implemented as double precision (f62).
     D(f64),
+    /// String data type.
+    ///
+    /// Implemented as String.
     S(String),
+    /// Tuple data type.
+    ///
+    /// Implemented as vector of tuple types (Vec<E>).
     T(Vec<E>),
+    /// None data type.
+    ///
+    /// In context of this tuple, None stands for the wild card that is used
+    /// for pattern matching when querying the tuple space for certain tuples.
     None,
+}
+
+impl E {
+	/// Prints an element to the standard output
+	/// # Examples
+	///
+	/// ```
+	/// # e.print()
+	/// ```
+	pub fn print(&self) {
+		match self {
+			&E::I(ref i) => print!("Int: {}, ", i),
+			&E::D(ref d) => print!("Double: {}, ", d),
+			&E::S(ref s) => print!("String: {}, ", s),
+			&E::T(ref v) => {
+				print!("Tuple: [");
+				for e in v {
+					e.print();
+				}
+				print!("], ");
+
+			}
+			&E::None => println!("Wildcard"),
+		}
+	}
 }
 
 /// Allow tuples to be equal to identical tuples with wildcards.
