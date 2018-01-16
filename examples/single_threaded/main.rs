45bf16aa0988f7174f614dfd3760ea73b887faf1
Michael Wagner <wagner.mchl@googlemail.com>
2018-01-17 00:01:19 +0800

    Changed: finished single threaded example

--- examples/single_threaded/main.rs
+++ examples/single_threaded/main.rs
@@ -5,45 +5,43 @@ use rustupolis::tuple::E;
 use rustupolis::tuple::Tuple;
 use rustupolis::tuplespace::TupleSpace;
 
-use rand::Rng;
+use rand::{Rng, Isaac64Rng, SeedableRng};
 
-fn put_and_read(mut t_space : TupleSpace) {
-    let mut rng = rand::thread_rng();
+fn put_and_read(mut rng: Isaac64Rng, mut t_space : TupleSpace) {
 
-    for _i in 0..10 {
+    for _i in 0..5 {
         println!("pushing tuple");
         let int = rng.gen::<i32>();
         let dbl = rng.gen::<f64>();
         let tup = Tuple::new(vec!(E::S("tuple".to_string()), E::I(int), E::D(dbl), E::S("more content...".to_string())), 99999);
         // tup.print();
-        for elem in tup.content {
-            elem.print();
-        }
-        t_space.put(tup)
+        println!("{:?}", tup);
+        t_space.put(tup);
     }
 
-    for _i in 0..10 {
+    for _i in 0..5 {
         println!("reading tuple");
-        let tup = t_space.take(Tuple::new(vec!(E::None, E::None, E::None, E::None), 0));
-        // tup.print();
-        match tup {
-            Some(x) => {
-                for elem in x.content {
-                    elem.print();
-                }
-            },
-            None => assert!(false)
-        }
+        let tup = t_space.read(Tuple::new(vec!(E::None, E::None, E::None, E::None), 0));
+        println!("{:?}", tup);
     }
 
 }
 
 fn main() {
 
+    // let mut rng = match OsRng::new() {
+    //     Ok(g) => g,
+    //     Err(e) => panic!("Failed to obtain OS RNG: {}", e)
+    // };
+
+    let seed: &[_] = &[1, 2, 3, 4];
+    let mut rng = rand::Isaac64Rng::new_unseeded();
+    rng.reseed(seed);
+
     println!("rustupolis - hello world example");
     let t_space = TupleSpace::new();
 
-    put_and_read(t_space);
+    put_and_read(rng, t_space);
 
     println!();
     println!();
