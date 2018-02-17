769bf59ab226f92ef199ecda7608097c41189cdc
Casey Marshall <me@cmars.tech>
2018-02-17 16:28:49 -0600

    A more concrete Match.

--- src/space.rs
+++ src/space.rs
@@ -6,7 +6,22 @@ use error::Error;
 use store::Store;
 use tuple::Tuple;
 
-pub type Match = Future<Item = Tuple, Error = Error>;
+pub struct Match {
+    want: Tuple,
+    have: Option<Tuple>,
+}
+
+impl Future for Match {
+    type Item = Tuple;
+    type Error = Error;
+
+    fn poll(&mut self) -> Result<Async<Tuple>, Error> {
+        match self.have {
+            Some(_) => Ok(Async::Ready(self.have.take().unwrap())),
+            None => Ok(Async::NotReady),
+        }
+    }
+}
 
 pub struct Space<T: Store> {
     store: T,
@@ -20,11 +35,11 @@ where
         Space { store: store }
     }
 
-    pub fn in_(&mut self, tup: Tuple) -> Box<Match> {
+    pub fn in_(&mut self, tup: Tuple) -> Match {
         panic!("todo");
     }
 
-    pub fn rd(&mut self, tup: Tuple) -> Box<Match> {
+    pub fn rd(&mut self, tup: Tuple) -> Match {
         panic!("todo");
     }
 
