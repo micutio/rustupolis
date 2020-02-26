10fec2ea54229169631ce410243f1a8ac1e8518e
Michael Wagner <wagner.mchl@googlemail.com>
2020-02-26 17:07:13 +0800

    added: cli main module stub

---
+++ src/bin/cli.rs
@@ -0,0 +1,15 @@
+//! # Rustupolis CLI
+//!
+//! A tuple space server implementation for Rust.
+//!
+//! TODO: Add more information
+//!
+//! This binary does ...
+
+extern crate rustupolis;
+
+mod cli;
+
+fn main() {
+    println!("Rustupolis CLI");
+}
